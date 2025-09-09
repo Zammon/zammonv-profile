681f3e6b4215b1da711d3d9aaeb2759866c589d9
ZAMMY <zammonv@ZammonBook.local>
2025-09-09 14:48:09 +0700

    [update] install react-router-dom, sass, setup script and setup component

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import CommissionPage from './Pages/commission';
 
 function App() {
+  
+  const paths = [
+    { path: "/", component: <CommissionPage />,},
+    { path: "/commission", component: <CommissionPage />,},
+  ]
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      {/* Main Routes */}
+      <Router>
+        <Routes>
+          { paths && paths.map((x) => (<Route path={x.path} element={x.component} />)) }
+        </Routes>
+      </Router>
+    </>
   );
 }
 
---
+++ src/Pages/commission.js
@@ -0,0 +1,7 @@
+export default function CommissionPage() {
+    return (
+        <div className="">
+            commission page
+        </div>
+    )
+}
